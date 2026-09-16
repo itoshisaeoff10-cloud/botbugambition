365b490b41d7c5eae8d6f4c0bb56a638eb7b9655
itoshisaeoff10-cloud <itoshisaeoff10@gmail.com>
2026-09-16 06:28:17 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    ownerId: 8446104582,//change your id telegram 
+    telegramBotToken: "YOUR BOT TOKEN",///the bot is being administered in your channel
+    sessionName: "session",
+    chanelid: "@Zami_Tech_Official_1",///change your username channel 
+    chatgrupid: "@Zami_MD_Updates",///change your grup public,do not grup private 
+    thumburl: "CAT BOX LINK YOUR PIC"
+};
